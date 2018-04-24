46640488d51e8814057445edb091f51df55fd452
jakelambert1 <0lambj41@solent.ac.uk>
2018-04-24 14:56:20 +0100

    SQLite half way through tutorial

---
+++ app/src/main/java/com/example/a0lambj41/budgetplannerapp/MyHelper.java
@@ -0,0 +1,47 @@
+package com.example.a0lambj41.budgetplannerapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "budget_planner.db";
+    public static final String TABLE_NAME = "transactions.db";
+    public static final String COL1 = "ID";
+    public static final String COL2 = "Name";
+    public static final String COL3 = "Amount";
+
+    public MyHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createTable = "CREATE TABLE " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOCRIMENT, "
+                + " NAME TEXT, EMAIL TEXT, TVSHOW TEXT)";
+        sqLiteDatabase.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP IF TABLE EXISTS " + TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean addData(String name, String amount){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2,name);
+        contentValues.put(COL3,amount);
+
+        long result = sqLiteDatabase.insert(TABLE_NAME, null, contentValues);
+
+        if(result == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
